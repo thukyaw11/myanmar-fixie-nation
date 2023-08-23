8c90f731a01077c38e49c90cbb0e5c3413eb12ff
thukyaw11 <helloeleven968@gmail.com>
2023-08-23 18:57:25 +0630

    fix: react version conflict with react scroll animate

--- src/components/ProfileCard.jsx
+++ src/components/ProfileCard.jsx
@@ -9,11 +9,9 @@ const ProfileCard = ({member}) => {
         />
       </div>
       <div class="p-5">
-        <a href="#">
           <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {member.name}
           </h5>
-        </a>
         <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
          {member.position}
         </p>
