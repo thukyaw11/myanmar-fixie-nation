301fd4263ce2c7419e33402d197dfaf347cd1811
thukyaw11 <helloeleven968@gmail.com>
2023-08-25 23:26:03 +0630

    feat: google analytics

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import "./App.css";
-import { useEffect } from "react";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import "animate.css/animate.min.css";
@@ -17,10 +16,8 @@ import ReactGA from 'react-ga4';
 ReactGA.initialize("G-4BQH6P1JX5");
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-  
+
+
   return (
 
     <div className="font-poppins bg-white dark:bg-[#0F172A]">
