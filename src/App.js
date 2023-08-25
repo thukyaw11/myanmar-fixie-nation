053e71f064bcf674f3c20dbc794ce6ab0cac2cee
thukyaw11 <helloeleven968@gmail.com>
2023-08-25 23:25:36 +0630

    feat: google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect } from "react";
 import Header from "./components/Header";
 import Landing from "./components/Landing";
 import "animate.css/animate.min.css";
@@ -16,7 +17,12 @@ import ReactGA from 'react-ga4';
 ReactGA.initialize("G-4BQH6P1JX5");
 
 function App() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+  
   return (
+
     <div className="font-poppins bg-white dark:bg-[#0F172A]">
       <Header />
       <div className="container mx-auto md:px-0 px-4">
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,8 +16,7 @@ const Footer = () => {
           </div>
           <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li>
-              <a
-                href=""
+              <div
                 class="text-gray-500 hover:text-gray-900 dark:hover:text-white"
               >
                 <svg
@@ -34,7 +33,7 @@ const Footer = () => {
                   />
                 </svg>
                 <span class="sr-only">Facebook page</span>
-              </a>
+              </div>
             </li>
           </ul>
         </div>
