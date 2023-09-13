19da12a9b4c0ba73ef0ca83f5d58d052115d93fc
thukyaw11 <helloeleven968@gmail.com>
2023-09-13 07:49:54 +0630

    fix: typo fixed

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ import ContactUs from "./components/ContactUs";
 import Footer from "./components/Footer";
 import ReactGA from "react-ga4";
 import { Helmet } from 'react-helmet'
-import FAQ from "./components/FAQ";
 
 ReactGA.initialize("G-4BQH6P1JX5");
 
@@ -43,7 +42,6 @@ function App() {
         <JoinUs />
         <HocPage />
         <ContactUs />
-        {/* <FAQ /> */}
       </div>
       <Footer />
     </div>
